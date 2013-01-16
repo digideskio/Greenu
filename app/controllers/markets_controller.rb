bb8e502b120ec8a1e73a7f0e7599e5f5970429af
bobbileehartman <bobbileehartman@gmail.com>
2013-01-16 12:46:51 -0600

    working through the logic once again, got it working on the show page finally

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,55 +1,64 @@
-require 'JSON'
+require 'json'
 require 'open-uri'
-
+ 
 class MarketsController < ApplicationController
   
   def index
-  	# @markets = Market.all
+    @markets = Market.all
   end
-
+ 
   def show
-    # @markets = Market.search(params[:search])
-  	if((params["zip"] == nil) || (params["zip"] == "00000") || (params["zip"].length != 5) || (params["zip"].match(/\D/) != nil))
-        flash[:notice] = "Invalid Zipcode"
-        redirect_to root_path
-        return
-    end
-
-    user_zip = params[:zip]
+    
+    user_zipcode = params[:search]
     zips = Market.pluck(:zipcode).uniq.join('|')
-
-    json_response = open(URI.encode("http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{user_zip}&destinations=#{zips}&language=en&sensor=false")).read
+ 
+    json_response = open(URI.encode("http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{user_zipcode}&destinations=#{zips}&language=en&sensor=false")).read
     markets = JSON.parse(json_response)
-
+ 
     paired_markets = markets['destination_addresses'].zip(markets['rows'].first['elements'])
+    
     close_markets = paired_markets.select do |destination, data|
-        data['distance']['value'] < 2500
+      data['distance']['value'] < 2500
     end
-
-    zipcodes = close_markets.map do |destination, data|
-        zip_code = destination[/(\d+)/]
-    end
-
-    distance = close_markets.map do |destination, data|
-        distance = data['distance']['text']
+ 
+    @distance = close_markets.map do |destination, data|
+                distance = data['distance']['text']
     end
     
-    @markets = Market.where(:zipcode => zipcodes)
-    @distance = distance 
+    @zipcodes = close_markets.map do |destination, data|
+                zip_code = destination[/(\d+)/]
+    end
 
-  	respond_to do |format|
+    @markets = Market.where(:zipcode => @zipcodes)
+
+    respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @market }
     end 
   end
-
+ 
   def create
+ 
+   # @search = params[:search]
+   #  if((@search == nil) || (@search == "00000") || (@search.length != 5) || (@search.match(/\D/) != nil))
+   #      flash[:notice] = "Invalid Zipcode"
+   #      redirect_to root_path
+   #      return
+   #  end 
+ 
+   #  # user_zip = @search
+ 
+   #  @market = Market.new
+   #  @market.zipcode = @search
+   #  @market.save
+ 
+   #  redirect_to :action => :show, :id => @market.id
   end
-
+ 
   # def edit
-  # 	@market = Market.find(params[:id])
-
-  # 	respond_to do |format|
+  #   @market = Market.find(params[:id])
+ 
+  #   respond_to do |format|
   #     if @market.update_attributes(params[:market])
   #       format.html { redirect_to @market }
   #       format.json { head :no_content }
@@ -59,10 +68,10 @@ class MarketsController < ApplicationController
   #     end
   #   end
   # end
-
+ 
   # def update 
-  # 	@market = Market.find(params[:id])
-
+  #   @market = Market.find(params[:id])
+ 
   #   respond_to do |format|
   #     if @market.update_attributes(params[:market])
   #       format.html { redirect_to @market }
@@ -73,26 +82,25 @@ class MarketsController < ApplicationController
   #     end
   #   end 
   # end
-
-
+ 
+ 
   # def new
-  # 	@market = Market.new
-
-  # 	respond_to do |format|
+  #   @market = Market.new
+ 
+  #   respond_to do |format|
   #     format.html # new.html.erb
   #     format.json { render json: @market }
   #   end
   # end
-
+ 
   # def destroy
- 	# @market = Market.find(params[:id])
+  # @market = Market.find(params[:id])
   #   @market.destroy
-
+ 
   #   respond_to do |format|
   #     format.html { redirect_to markets_url }
   #     format.json { head :no_content }
-  # 	end
+  #   end
   # end
-
+ 
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Greenu::Application.routes.draw do
-  get "/markets" => "Markets#index", :as => "markets"
-
-  get "/market" => "Markets#show", :as => "market"
-
-  post "/market" => "Markets#create"
-
-  get "/market/edit" => "Markets#edit", :as => "edit_market"
-
-  get "/market/new" => "Markets#new", :as => "new_market"
-
+ 
+  get "/markets" => "markets#index", :as => "markets"
+ 
+  get "/market/new" => "markets#new", :as => "new_market"
+ 
+  get "/market/" => "markets#show", :as => "market"
+ 
+  post "/markets" => "markets#create"
+ 
+  # get "/market/edit" => "markets#edit", :as => "edit_market"
+ 
   get "/" => "Pages#home", :as => "home"
-
+ 
   root :to => "Pages#home"
-
+ 
 end
+ 
 
   
